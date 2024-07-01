176ca3e4d4cc83ccba21b9157bfadf78e29a765c
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-07-01 18:00:49 -0300

    desafio 4 atividades

---
+++ Lógica de programação - explore funções e listas/desafio4/atv1a6.js
@@ -0,0 +1,32 @@
+//1. Crie uma lista vazia, com o nome listaGenerica.
+
+let listaGenerica = [];
+
+
+
+//2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+//3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+let linguagensDeProgramacao = ['Javascript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+
+
+//4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+let listaTresTimes = ['Santos', 'São Paulo', 'Palmeiras'];
+console.log(listaTresTimes[0]);
+
+
+
+//5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+let listaTresCarros = ['Opala', 'Uno', 'Onix'];
+console.log(listaTresCarros[1]);
+
+
+
+//6. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+let listaTresPlantas = ['Acácia', 'Rosa', 'Ipê Roxo'];
+console.log(listaTresPlantas[2]);
