b1fcf1909451ff920b63c288519fe2af626731f2
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2024-07-04 16:53:31 -0300

    Desafio Portfólio

---
+++ HTML e CSS/Portfólio/script.js
@@ -0,0 +1,7 @@
+document.getElementById('nome').innerText = 'Daniel Wesley';
+document.getElementById('descricao').innerHTML = `
+    <strong>
+        Oi! Sou estudante de Software/Web Development e QA. <br>
+        Faço parte do programa Oracle Next Education.
+    </strong>
+`;
